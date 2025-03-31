c9b7c24be2f6e042256c5f2acd4e20aead782cec
Aman7902k <amankhan9034820992@gmail.com>
2025-03-31 22:47:26 +0530

    login page final

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,27 +2,26 @@ import React from 'react'
 
 function Login() {
   return (
-    <div className="pt-20 absolute inset-0 bg-gradient-to-r from-blue-800 via-purple-700 to-pink-600 flex justify-center h-[100vh] items-center">
-        <div className='flex flex-col pl-6  rounded  justify-center w-[40%]  mt-8'>
+      <div className='mt-18 bg-gradient-to-r from-blue-800 via-purple-700 to-pink-600 w-[100%] h-[100vh] flex justify-center'>
+        <div className='"relative z-10 max-w-3xl px-10 py-2 mt-4 backdrop-blur-lg bg-white/10 border h-[80%] border-white/20 rounded-2xl shadow-2xl'>
         <h2 className='w-[100%] text-center mt-2'>Welcome Back</h2>
-        <form action="#" className='w-[100%] p-4 flex flex-col gap-2 justify-center'>
+        <form action="#" className='w-[100%] p-4 flex flex-col gap-3'>
           <label htmlFor="email">Email Address</label>
           <input type="email" className='border-1 rounded p-1 ' name="email" id="email" placeholder='example@gmail.com' />
           <label htmlFor="pswd">Password</label>
           <input type="password" className='border-1 rounded p-1  ' name="pswd" id="pswd" placeholder='Enter Password' />
-          <div className='flex px-2 justify-between'>
+            <div className='text-end'>Forgot Password ?</div>
             <div >
                 <input className='mr-3' type="radio" name="remme" id="remme" />
                 <label htmlFor="remme">Remember Me</label>
             </div>
-            <span className=''>Forgot Password</span>
-          </div>
-          <button className='border-1 rounded-2xl bg-[#d4b8b8] p-1'>Login</button>
+          <button className='border-1 rounded-2xl bg-[] w-[80%] mx-auto p-1'>Login</button>
           <p className='w-[100%] text-center'>Didn't have an Account ? <span className='text-blue-600 hover:cursor-pointer'>Sign Up Here</span> </p>
         </form>
+        </div>
       </div>
-    </div>
   )
 }
 
 export default Login
+
